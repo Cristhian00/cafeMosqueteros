a96fe28bdd9f4b378cdd812fcb3d3f6fc595e6f7
Cristhian Ortiz <cortizm_1@uqvirtual.edu.co>
2022-04-12 00:37:23 -0500

    Agregue dos query en compraRepo

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/CompraRepo.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/CompraRepo.java
@@ -2,8 +2,18 @@ package co.edu.uniquindio.proyecto.repositorios;
 
 import co.edu.uniquindio.proyecto.entidades.Compra;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
 
 @Repository
 public interface CompraRepo extends JpaRepository<Compra, Integer> {
+
+    @Query("SELECT c FROM Compra c WHERE c.socioCompra.cedula = ?1")
+    List<Compra> obtenerCompras(String cedula);
+
+    @Query("SELECT c FROM Compra c WHERE c.fecha = ?1")
+    List<Compra> obtenerCompras(Date fecha);
 }
