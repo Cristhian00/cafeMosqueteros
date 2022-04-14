e7441fc23f6148307f355183e01a5497b1bbd210
valencia921 <dmvalenciah_1@uqvirtual.edu.co>
2022-04-14 09:55:18 -0500

    guardado

--- persistencia/src/test/java/co/edu/uniquindio/proyecto/test/SocioTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/proyecto/test/SocioTest.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.proyecto.test;
 
+import co.edu.uniquindio.proyecto.entidades.Socio;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,8 +12,12 @@ public class SocioTest {
 
     @Autowired
     private SocioRepo socioRepo;
+
     @Test
     public void registrarSocioTest(){
+        Socio socio = new Socio();
+        socio.setCedula("432");
+        socio.setContrasenia("3232");
 
     }
     @Test
