ca857612b459809b8b0b412c5c81f2c1dd32618f
Cristhian Ortiz <cortizm_1@uqvirtual.edu.co>
2022-06-03 15:36:54 -0500

    Se creo la primer clase del test

---
+++ web/src/test/java/co/edu/uniquindio/proyecto/rest/SocioRestControllerTest.java
@@ -0,0 +1,33 @@
+package co.edu.uniquindio.proyecto.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SocioRestControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listarSocios() {
+    }
+
+    @Test
+    void obtenerSocio() {
+    }
+
+    @Test
+    void crearSocio() {
+    }
+
+    @Test
+    void eliminarSocio() {
+    }
+
+    @Test
+    void actualizarSocio() {
+    }
+}
